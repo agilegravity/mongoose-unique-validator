b03dc8831a49b4811b01994aa6fc8dcdb5156155
Benedikt Weiner <weiner@agilegravity.com>
2022-03-07 21:33:06 +0100

    handel mongodb-client-encryption realated error, in config, not resolve(true) this error will be not promoted to the Application

--- index.js
+++ index.js
@@ -112,9 +112,29 @@ const plugin = function(schema, options) {
                                 model = model.db.model(model.baseModelName);
                             }
 
-                            model.find(conditions).countDocuments((err, count) => {
-                                resolve(count === 0);
-                            });
+                            // Skip, if only the _id field should be checked and it's a command on a existing document.
+                            let skip = false;
+                            if (!isNew && !isQuery) {
+                                const fieldsToCheck = Object.keys(conditions);
+
+                                if (fieldsToCheck.length === 1 && fieldsToCheck[0] === '_id') {
+                                    skip = true;
+                                }
+                            }
+
+                            if (skip) {
+                                resolve(true);
+                            } else {
+                                model.find(conditions).countDocuments((err, count) => {
+                                    if(err && err.code === 31122) {
+                                        // mongodb-client-encryption realated error, in config, not resolve(true) this error will be not promoted to the Application
+                                        resolve(true)
+                                    }
+                                    resolve(count === 0);
+                                });
+                            }
+
+                            
                         });
                     }, pathMessage, type);
                 }
